fbde3680756156c6b7108fba8ae20bded8ddce9b
Lukasz Korycki <korycki.lukas@gmail.com>
2018-11-29 02:56:14 -0500

    final fixes

--- src/scala/Hello.scala
+++ src/scala/Hello.scala
@@ -1,7 +1,8 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.{Encoder, SparkSession}
+import org.apache.spark.sql.{DataFrame, Encoder, SparkSession}
 import org.apache.spark.ml.linalg.{Vector, Vectors}
 import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
+import org.apache.spark.sql.functions._
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
@@ -27,10 +28,12 @@ object Hello {
 
     val inputRows = spark.sparkContext
       .textFile(arffPath)
-      .repartition(1)
+      .repartition(4)
       .filter((line: String) => !line.startsWith("@"))
       .map(_.split(",").to[List])
-      .map(toInstance)
+      .zipWithUniqueId()
+      .map((columnsWithId) => toInstance(columnsWithId._1, columnsWithId._2))
+
 
     println(inputRows.getNumPartitions)
 
@@ -44,10 +47,10 @@ object Hello {
     val k = spark.sparkContext.broadcast(K)
 
     val result = modelDS
-      .mapPartitions((it: Iterator[Instance]) => findPartitionNearestNeighbors(it, testData.value, k.value))(nearestNeighborsEncoder)
-      .groupByKey((nn: NearestNeighbors) => nn.instanceIdx)
-      .reduceGroups((nn1: NearestNeighbors, nn2: NearestNeighbors) => reducePartitionNearestNeighbors(nn1, nn2, k.value))
-      .map((instNN: (Int, NearestNeighbors)) => classify(instNN._1, instNN._2, testData.value))
+      .mapPartitions((modelDataIt: Iterator[Instance]) => findPartitionNearestNeighbors(modelDataIt, testData.value, k.value))(nearestNeighborsEncoder)
+      .groupByKey((nn: NearestNeighbors) => nn.instanceId)
+      .reduceGroups((nn1: NearestNeighbors, nn2: NearestNeighbors) => reducePartitionNearestNeighbors(nn1, nn2, k.value)) // does not run if one partition
+      .map(nn => classify(nn._2))
       .reduce(_ + _)
 
     val duration = (System.nanoTime - startTime) / 1e9d
@@ -58,55 +61,58 @@ object Hello {
 
   }
 
-  def toInstance(rawColumns: List[String]): Instance = Instance(
+  def toInstance(rawColumns: List[String], id: Long): Instance = Instance(
+    id,
     Vectors.dense(rawColumns.dropRight(1).map(l => l.toDouble).toArray),
     rawColumns.last.toInt
   )
 
-  def findPartitionNearestNeighbors(it: Iterator[Instance], testData: Array[Instance], k: Int): Iterator[NearestNeighbors] = {
+  def findPartitionNearestNeighbors(modelDataIt: Iterator[Instance], testData: Array[Instance], k: Int): Iterator[NearestNeighbors] = {
     val partitionNeighbors: ArrayBuffer[NearestNeighbors] = ArrayBuffer()
     val instancesPartitionNeighbors: ArrayBuffer[ArrayBuffer[ClassDist]] = ArrayBuffer.fill(testData.length)(ArrayBuffer())
 
-    while (it.hasNext) {
-      val modelInstance = it.next()
+    while (modelDataIt.hasNext) {
+      val modelInstance = modelDataIt.next()
 
       for ((testInstance: Instance, i: Int) <- testData.zipWithIndex) {
+        if (testInstance.id != modelInstance.id) {
           val dist = Vectors.sqdist(testInstance.features, modelInstance.features)
           instancesPartitionNeighbors(i) += ClassDist(modelInstance.label, dist)
+        }
       }
     }
 
     for ((testInstance: Instance, i: Int) <- testData.zipWithIndex) {
-      partitionNeighbors += NearestNeighbors(i, instancesPartitionNeighbors(i).sortBy((cd: ClassDist) => cd.dist).take(k))
+      partitionNeighbors += NearestNeighbors(
+        testInstance.id,
+        instancesPartitionNeighbors(i).sortWith((cd1: ClassDist, cd2: ClassDist) => cd1.dist < cd2.dist).take(k),
+        testInstance.label
+      )
     }
 
     partitionNeighbors.iterator
   }
 
   def reducePartitionNearestNeighbors(nn1: NearestNeighbors, nn2: NearestNeighbors, k: Int): NearestNeighbors = {
-    val mergedNearestNeighbors: ArrayBuffer[ClassDist] = ArrayBuffer.fill(k)(null)
+    val mergedNearestNeighbors: ArrayBuffer[ClassDist] = (nn1.neighbors ++ nn2.neighbors)
+      .sortWith((cd1: ClassDist, cd2: ClassDist) => cd1.dist < cd2.dist).take(k)
 
-    (0 until k).foreach(i => {
-      mergedNearestNeighbors(i) = if (nn1.neighbors(i).dist < nn2.neighbors(i).dist) nn1.neighbors(i) else nn2.neighbors(i)
-    })
-
-    NearestNeighbors(nn1.instanceIdx, mergedNearestNeighbors)
+    NearestNeighbors(nn1.instanceId, mergedNearestNeighbors, nn1.trueLabel)
   }
 
-  def classify(instanceId: Int, instanceNearestNeighbors: NearestNeighbors, testData: Array[Instance]): Int = {
+  def classify(instanceNearestNeighbors: NearestNeighbors): Int = {
     val votes: mutable.Map[Int, Int] = mutable.Map[Int, Int]()
 
     for (vote <- instanceNearestNeighbors.neighbors) {
       if (!votes.contains(vote.label)) {
         votes.put(vote.label, 1)
       } else {
-        votes.put(votes(vote.label), votes(vote.label) + 1)
+        votes(vote.label) = votes(vote.label) + 1
       }
     }
 
     val prediction = votes.maxBy(_._2)._1
-    println(prediction, testData(instanceId).label)
-    if (prediction == testData(instanceId).label) 1 else 0
+    if (prediction == instanceNearestNeighbors.trueLabel) 1 else 0
   }
 
   val instanceEncoder: Encoder[Instance] = ExpressionEncoder.apply[Instance]()
@@ -114,6 +120,6 @@ object Hello {
 
 }
 
-case class Instance(features: Vector, label: Int)
-case class NearestNeighbors(instanceIdx: Int, var neighbors: ArrayBuffer[ClassDist])
+case class Instance(id: Long, features: Vector, label: Int)
+case class NearestNeighbors(instanceId: Long, var neighbors: ArrayBuffer[ClassDist], trueLabel: Int)
 case class ClassDist(label: Int, dist: Double)
