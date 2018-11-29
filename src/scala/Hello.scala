1f6b31edfffa5ee0d9b67d8bdac320fd4a963501
Lukasz Korycki <korycki.lukas@gmail.com>
2018-11-28 21:30:41 -0500

    pipeline poc

--- src/scala/Hello.scala
+++ src/scala/Hello.scala
@@ -1,32 +1,19 @@
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
-import org.apache.spark.ml.linalg.SQLDataTypes.VectorType
+import org.apache.spark.sql.{Encoder, SparkSession}
 import org.apache.spark.ml.linalg.{Vector, Vectors}
-
-import scala.io.Source
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
 
 object Hello {
 
   def main(args: Array[String]) {
 
+    val instanceEncoder: Encoder[Instance] = ExpressionEncoder.apply[Instance]()
+    val nearestNeighborsEncoder: Encoder[NearestNeighbors] = ExpressionEncoder.apply[NearestNeighbors]()
+
     val arffPath = "data\\small.arff"
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
     //val log = Logger.getLogger(getClass.getName)
-
-    def schema: StructType =
-      StructType(
-        Seq(
-          StructField(name = "features", dataType = VectorType, nullable = false),
-          StructField(name = "class", dataType = IntegerType, nullable = false)
-        )
-      )
-
-    def toRow(rawColumns: List[String]): Row = Row(
-      Vectors.dense(rawColumns.dropRight(1).map(l => l.toDouble).toArray),
-      rawColumns.last.toInt
-    )
 
     val spark = SparkSession
       .builder()
@@ -34,30 +21,47 @@ object Hello {
       .master("local[*]")
       .getOrCreate()
 
+    import spark.implicits._
+
     val inputRows = spark.sparkContext
       .textFile(arffPath)
+      .repartition(8)
       .filter((line: String) => !line.startsWith("@"))
       .map(_.split(",").to[List])
-      .map(toRow)
+      .map(toInstance)
 
-    val inputDF = spark.createDataFrame(inputRows, schema)
+    println(inputRows.getNumPartitions)
 
-    inputDF.printSchema()
-    inputDF.show()
+    val inputDS = spark.createDataset(inputRows)(instanceEncoder)
+    inputDS.printSchema()
+    inputDS.show()
+    //inputDF.cache()
 
-//    val numAs = logData.filter(line => line.contains("a")).count()
-//    val numBs = logData.filter(line => line.contains("b")).count()
-//    println(s"Lines with a: $numAs, Lines with b: $numBs")
+    val testData = spark.sparkContext.broadcast(inputDS.collect())
+
+    val result = inputDS.mapPartitions((it: Iterator[Instance]) => {
+      println("dupa " + testData)
+
+      val nearestsNeighbors = Array(NearestNeighbors(0, Array()))
+
+      nearestsNeighbors.iterator
+    })(nearestNeighborsEncoder)
+      .groupByKey((nn: NearestNeighbors) => nn.instanceIdx)
+      .reduceGroups((nn1: NearestNeighbors, nn2: NearestNeighbors) => nn1)
+      .map(t => 1)
+      .reduce((a,b) => 0) // incremental average
+
 
     spark.stop()
 
   }
 
-  def parseFile(path: String): Unit = {
-
-    for (line <- Source.fromFile(path).getLines) {
-      println(line)
-    }
-  }
+  def toInstance(rawColumns: List[String]): Instance = Instance(
+    Vectors.dense(rawColumns.dropRight(1).map(l => l.toDouble).toArray),
+    rawColumns.last.toInt
+  )
 
 }
+
+case class Instance(features: Vector, label: Int)
+case class NearestNeighbors(instanceIdx: Int, neighbors: Array[(Int, Double)])
